e9519229a20687a77ff5d1aec513804042ea6eb6
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2024-07-24 19:18:36 +0530

    added test support using env variables

--- autovid/utils/reddit_handler.py
+++ autovid/utils/reddit_handler.py
@@ -1,4 +1,5 @@
 import praw
+import os
 
 from autovid import api_keys
 from autovid import exceptions
@@ -6,10 +7,19 @@ from autovid import exceptions
 __all__ = ['_fetch_post', '_fetch_comment']
 
 def __getcredentials():
+    
+    # Testing ONLY
+    if not api_keys.client_id:
+        return praw.Reddit(
+        client_id=os.getenv('REDDIT_CLIENT_ID'),
+        client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
+        user_agent="autovid 0.0.2"
+        )
+
     return praw.Reddit(
         client_id=api_keys.client_id,
         client_secret=api_keys.client_secret,
-        user_agent="autovid 0.0.1"
+        user_agent="autovid 0.0.2"
     )
 
 def _fetch_post(url:str):
--- tests/test_reddit_handler.py
+++ tests/test_reddit_handler.py
@@ -11,8 +11,8 @@ def test_no_api_keys():
 #     with pytest.raises(Exception):
 #         clip = av.redditpostclip("https://www.invalid_url.com")
 
-# def test_valid_post_url():
-#     try:
-#         clip = av.redditpostclip("https://www.reddit.com/r/reddit.com/comments/87/the_downing_street_memo/")
-#     except Exception:
-#         pytest.fail("Unexcepted exception thrown")
+def test_valid_post_url():
+    try:
+        clip = av.redditpostclip("https://www.reddit.com/r/reddit.com/comments/87/the_downing_street_memo/")
+    except Exception:
+        pytest.fail("Unexcepted exception thrown")
