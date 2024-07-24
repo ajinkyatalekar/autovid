4c46d205750588d541e0d8b277388be65c917979
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2024-07-24 15:46:03 +0530

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import os
 
 VERSION = '0.0.1'
 DESCRIPTION = 'Generate Reddit Text-to-Speech videos'
-LONG_DESCRIPTION = 'A package that allows to quickly create videos from raw text or fetching reddit posts and comments.'
 
 # Setting up
 setup(
@@ -12,13 +11,10 @@ setup(
     author="Ajinkya Talekar",
     author_email="ajinkyat@buffalo.edu",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
     packages=find_packages(),
     install_requires=['selenium', 'moviepy', 'gtts', 'praw'],
     keywords=['python', 'reddit', 'tts', 'video generator', 'automation'],
     classifiers=[
-        "Development Status :: 2 - Development",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
