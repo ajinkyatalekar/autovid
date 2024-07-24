92923cd13f56d8fcddb29b378b2cbff4ba856dd9
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2024-07-24 19:43:52 +0530

    updated tests

--- tests/test_reddit_handler.py
+++ tests/test_reddit_handler.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 import autovid as av
 
@@ -7,12 +8,14 @@ def test_no_api_keys():
         av.api_keys.client_id = ""
         clip = av.redditpostclip("https://www.reddit.com/r/reddit.com/comments/87/the_downing_street_memo/")
 
+def test_valid_post_url():
+    try:
+        av.api_keys.client_id = os.getenv('REDDIT_CLIENT_ID')
+        av.api_keys.client_secret = os.getenv('REDDIT_CLIENT_SECRET')
+        clip = av.redditpostclip("https://www.reddit.com/r/reddit.com/comments/87/the_downing_street_memo/")
+    except Exception:
+        pytest.fail("Unexcepted exception thrown")
+
 # def test_invalid_post_url():
 #     with pytest.raises(Exception):
 #         clip = av.redditpostclip("https://www.invalid_url.com")
-
-# def test_valid_post_url():
-#     try:
-#         clip = av.redditpostclip("https://www.reddit.com/r/reddit.com/comments/87/the_downing_street_memo/")
-#     except Exception:
-#         pytest.fail("Unexcepted exception thrown")
