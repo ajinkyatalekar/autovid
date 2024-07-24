59019be344840ba75226f085bd6462e594ae2308
Ajinkya Talekar <91043799+ajinkyatalekar@users.noreply.github.com>
2024-07-24 19:23:53 +0530

    bug fix

--- autovid/utils/reddit_handler.py
+++ autovid/utils/reddit_handler.py
@@ -7,14 +7,6 @@ from autovid import exceptions
 __all__ = ['_fetch_post', '_fetch_comment']
 
 def __getcredentials():
-    
-    # Testing ONLY
-    if not api_keys.client_id:
-        return praw.Reddit(
-        client_id=os.getenv('REDDIT_CLIENT_ID'),
-        client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-        user_agent="autovid 0.0.2"
-        )
 
     return praw.Reddit(
         client_id=api_keys.client_id,
